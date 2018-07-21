0ca3635595fe53ca1e84943ae09bf6a4b4286fdb
Nick Rodriguez <endotnick@gmail.com>
2018-07-21 14:40:14 -0700

    completes lab 3

--- Lab3Files/ContestLab.java
+++ Lab3Files/ContestLab.java
@@ -2,7 +2,7 @@
 Programmer: Nick Rodriguez
 Description: This program defines a ContestantRecord object and
    creates an ArrayList<ContestantRecord> from the file.
-Time spent on assignment 3: 3 hours, 17 minutes
+Time spent on assignment 3: 4 hours, 49 minutes
 */
 import java.util.*;
 import java.io.*;
@@ -10,14 +10,19 @@ import java.io.*;
 class ContestLab{
    public static final int REPORT_WIDTH = 50;
    public static final int CONTESTANT_WIDTH = 29;
-   public static final String REPORT_HEADING = String.format("\t%s\t\t\t\t\t\t\t  %s\t\t %s", "Contestants", "Ft", "Secs");
+   public static final String REPORT_HEADING = String.format("\t%s\t\t\t\t\t" +
+      "\t\t  %s\t\t %s", "Contestants", "Ft", "Secs");
 
    public static void main(String[] args) throws IOException{
-      Scanner console = new Scanner(System.in);
       String fileName = "contest2016.dat";
-      ContestantRecord targetRec;
       ArrayList<ContestantRecord> data = getData(fileName);
-      
+
+      testRecords(data);
+      testSearch(data);
+   }
+
+   // Sorts ArrayList using various methods and prints the result
+   public static void testRecords(ArrayList<ContestantRecord> data) {
       System.out.println(data);
       displayReport("Original Data", data);
       Collections.shuffle(data);
@@ -28,93 +33,91 @@ class ContestLab{
       displayReport("Race Results", data);
       Collections.sort(data, new TossComparator());
       displayReport("Results by Toss Distance", data);
-      
+   }
+
+   // Sorts a given ArrayList alphabetically, searches for user provided entry
+   public static void testSearch(ArrayList<ContestantRecord> data) {
+      Scanner console = new Scanner(System.in); 
+      ContestantRecord targetRec;
+      int indexOf;
+      int binarySearch;
+
+      Collections.sort(data);
+
       do {
          System.out.print("Enter target name to search for (or QUIT): ");
          targetRec = new ContestantRecord();
          targetRec.contestant = console.nextLine();
-         if (data.indexOf(targetRec) > 0){
-            targetRec = data.get(data.indexOf(targetRec));
-            System.out.printf("indexOf() shows %s had toss of %.1f ft and race time of %.1f secs\n", targetRec.contestant, targetRec.distance, targetRec.time);
+         indexOf = data.indexOf(targetRec);
+         binarySearch = Collections.binarySearch(data, targetRec);
+
+         if (binarySearch >= 0 && indexOf == binarySearch){
+            targetRec = data.get(indexOf);
+            System.out.printf("indexOf() shows %s had toss of %.1f ft and " +
+               "race time of %.1f secs\n", targetRec.contestant,
+               targetRec.distance, targetRec.time);
+            targetRec = data.get(binarySearch);
+            System.out.printf("binarySearch() method shows %s had toss of " +
+               "%.1f ft and race time of %.1f secs\n", targetRec.contestant,
+               targetRec.distance, targetRec.time);
          } else {
-            System.out.printf("indexOf( says %s NOT in list.\n", targetRec.contestant);
-         }            
+            System.out.printf("indexOf() says %s NOT in list.\n",
+               targetRec.contestant);
+            System.out.printf("binarySearch() method says %s NOT in list.\n",
+               targetRec.contestant);
+         }
       }while(!targetRec.contestant.equalsIgnoreCase("QUIT"));
    }
-   
+
+   // Given an ArrayList<ContestantRecord>, prints formatted data to console
    private static void displayReport(String title, ArrayList<ContestantRecord> data) {
       printChar(System.out, ' ', (REPORT_WIDTH / 2) - (title.length() / 2));
       System.out.printf("%s\n", title);
-      printChar(System.out, '=', REPORT_WIDTH);     
+      printChar(System.out, '=', REPORT_WIDTH);
       System.out.printf("\n%s\n", REPORT_HEADING);
-      printChar(System.out, '=', REPORT_WIDTH);  
+      printChar(System.out, '=', REPORT_WIDTH);
       for (ContestantRecord record : data) {
          System.out.printf("\n\t%s", record.contestant);
          printChar(System.out, ' ', CONTESTANT_WIDTH - record.contestant.length() + 1);
-         System.out.printf("%.2f\t\t %.2f", record.distance, record.time);         
+         System.out.printf("%.2f\t\t %.2f", record.distance, record.time);
       }
       System.out.println();
-      
    }
    
+   // Reads file and returns contents as ArrayList
    private static ArrayList<ContestantRecord> getData(String fileName) throws IOException{
       Scanner file = new Scanner(new File(fileName));
       ArrayList<ContestantRecord> data = new ArrayList<ContestantRecord>();
-      
+
       while(file.hasNext()) {
          ContestantRecord record = new ContestantRecord();
-         record.setName(file.nextLine());       //or: record.contestant = ...
-         record.setDistance(file.nextDouble()); //or: record.distance = ...
-         record.setTime(file.nextDouble());     //or: record.time = ...
+         record.contestant = file.nextLine();
+         record.distance = file.nextDouble();
+         record.time = file.nextDouble();
          data.add(record);
          file.nextLine();
       }
-      
       return data;
    }
-   
-   // prints the passed char to the console 'count' times.
+
+   // Prints the passed char to the console 'count' times.
    private static void printChar(PrintStream output, char c, int count){
       for (int i = 1; i <= count; i++) {
          output.print(c);
       }
    }
 
-   /* Internal Classes */         
+   /****** Internal Classes  ******/
    public static class ContestantRecord implements Comparable<ContestantRecord>{
       private String contestant;
       private double distance;
       private double time;
-      
-      /* Constructors */
-      public ContestantRecord() {
-         this("", 0.0, 0.0);
-      }
-      
-      public ContestantRecord( String contestant, double distance, double time) {
-         setName(contestant);
-         setDistance(distance);
-         setTime(time);
-      }
-      
-      /* Mutators */
-      public void setName(String contestant) {
-         this.contestant = contestant;
-      }
-      
-      public void setDistance(double distance) {
-         this.distance = distance;
-      }
-      
-      public void setTime(double time) {
-         this.time = time;
-      }
-      
+
       /* Accessors */
       public String toString() {
          return String.format("%s %.2f %.2f\n", this.contestant, this.distance, this.time);
       }
-      
+
       /* Overrides */
       public boolean equals(Object obj) {
          if (obj instanceof ContestantRecord) {
@@ -122,12 +125,12 @@ class ContestLab{
          }
          return false;
       }
-      
+
       public int compareTo(ContestantRecord c1) {
          return contestant.toLowerCase().compareTo(c1.contestant.toLowerCase());
       }
    }
-   
+
    public static class TossComparator implements Comparator<ContestantRecord> {
       public int compare(ContestantRecord c1, ContestantRecord c2) {
          Double d1 = new Double(c1.distance);
@@ -143,6 +146,4 @@ class ContestLab{
          return t1.compareTo(t2);
       }
    }
-
-   
 }
